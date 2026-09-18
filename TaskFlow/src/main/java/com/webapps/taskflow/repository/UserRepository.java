f859156882970248f864b49b1e643b9807efb52a
Galileu Calaça Menezes de Moraes <galileumcalaca@gmail.com>
2026-09-18 13:16:18 -0300

    feat: enforce unique user email and add repository lookup

--- TaskFlow/src/main/java/com/webapps/taskflow/repository/UserRepository.java
+++ TaskFlow/src/main/java/com/webapps/taskflow/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.webapps.taskflow.repository;
 import com.webapps.taskflow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
